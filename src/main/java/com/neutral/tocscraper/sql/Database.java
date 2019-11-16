0c3a56208e5a22ad562ca1412fef8a58dbb5b033
Thanas Pulaj <asice1@yahoo.com>
2019-11-16 13:16:05 +0100

    Database improvements

--- src/main/java/com/neutral/tocscraper/sql/Database.java
+++ src/main/java/com/neutral/tocscraper/sql/Database.java
@@ -5,6 +5,7 @@ import com.neutral.tocscrapermodels.Chapter;
 import com.neutral.tocscrapermodels.ChapterContainer;
 import com.neutral.tocscrapermodels.DBRow;
 import com.neutral.tocscrapermodels.Novel;
+import static com.neutral.tocscrapermodels.Novel.removeExtra;
 import com.neutral.tocscrapermodels.NovelContainer;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -28,7 +29,7 @@ public class Database {
     private String novelId = " novel_id.'_id' ";
     private String novelName = " novel_id.name ";
     private String novelNovel = " novel_chapters.novel ";
-    private String novelChapter = " novel_chapters.chapter ";
+    private String novelChapter = " novel_chapters.title ";
     private String novelLink = " novel_chapters.link ";
 
     public Database() {
@@ -37,9 +38,9 @@ public class Database {
 //            String dbUser = db[0].split(":")[0];
 //            String dbPass = db[0].split(":")[1];
 //            String dbLink = db[1];
-            String dbUser = "Iwkn0Ill0U";
-            String dbPass = "2FVdgXV3H9";
-            String dbLink = "remotemysql.com:3306/Iwkn0Ill0U";
+            String dbUser = "WAOOiOHHaC";
+            String dbPass = "GWz5r06J31";
+            String dbLink = "remotemysql.com:3306/WAOOiOHHaC";
             conn = DriverManager.getConnection("jdbc:mysql://" + dbLink, dbUser, dbPass);
 
         } catch (SQLException e) {
@@ -51,6 +52,7 @@ public class Database {
 
     public void updateDB(NovelContainer novelsInMemory) {
         try (Statement statement = conn.createStatement();) {
+            //Get novel_id, novel_name, chapter_link, chapter_title
             ResultSet results = statement.executeQuery(
                     "SELECT\n"
                     + "    novel_id._id AS novel_id,\n"
@@ -64,6 +66,7 @@ public class Database {
                     + "    novel_id.name");
             List<DBRow> rows = new ArrayList<>();
 
+            //Create a DBRow entity for every DB row
             while (results.next()) {
                 rows.add(new DBRow(results.getString(1), StringEscapeUtils.escapeEcmaScript(results.getString(2)), results.getString(3), results.getString(4)));
             }
@@ -76,10 +79,13 @@ public class Database {
             NovelContainer novelsToBeUpdated = new NovelContainer();
             ChapterContainer chaptersToBeUpdated = new ChapterContainer();
 
+            //Iterate through every row in the DB
             for (var row : rows) {
+                //If the database contains a scraped novel
                 if (novelsInMemory.contains(row.getNovelTitle())) {
                     Scraper.LOGGER.log(Level.FINEST, "{0} is already in the DB.", row.getNovelTitle());
-                } else if (novelsInMemory.contains(row.getNovelTitle().trim() + " (Completed)")
+                } //If the database contains an incomplete scraped novel
+                else if (novelsInMemory.contains(row.getNovelTitle().trim() + " (Completed)")
                         && !novelsToBeUpdated.contains(novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Completed)").getTitle())) {
                     statement.addBatch("UPDATE novel_id SET name = '"
                             + novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Completed)")
@@ -87,16 +93,29 @@ public class Database {
                             + row.getNovelId() + "'");
                     novelsToBeUpdated.add(novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Completed)"));
                     Scraper.LOGGER.log(Level.FINEST, "{0} Novel completed, updated title.", row.getNovelTitle());
-                } else {
+                } //If the database contains an unsuspended scraped novel
+                else if (novelsInMemory.contains(row.getNovelTitle().trim() + " (Suspend)")
+                        && !novelsToBeUpdated.contains(novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Suspend)").getTitle())) {
+                   statement.addBatch("UPDATE novel_id SET name = '"
+                            + novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Suspend)")
+                            + "' WHERE _id ='"
+                            + row.getNovelId() + "'");
+                    novelsToBeUpdated.add(novelsInMemory.getNovelByName(row.getNovelTitle().trim() + " (Suspend)"));
+                    Scraper.LOGGER.log(Level.FINEST, "{0} Novel suspended, updated title.", row.getNovelTitle());
+                } //If the database doesn't contains a novel
+                else {
                     continue;
                 }
-                ChapterContainer chaptersInMemory = chaptersInMemory
-                        = novelsInMemory.getNovelByNameIgnoreCompleted(row.getNovelTitle()).getChapters();
 
+                //Get all scraped chapters of the novel with the current title, ignoring completion status
+                ChapterContainer chaptersInMemory = novelsInMemory.getNovelByNameIgnoreStatus(row.getNovelTitle()).getChapters();
+                //If scraped chapters of this novel contain this specific chapter
                 if (chaptersInMemory.contains(row.getChapterTitle())) {
+                    //If the scraped chapter link is the same as the DB link
                     if (chaptersInMemory.getChapterByTitle(row.getChapterTitle()).getLink().equals(row.getChapterLink())) {
                         Scraper.LOGGER.log(Level.FINEST, "{0}: {1} is already in the DB.", new Object[]{row.getNovelTitle(), row.getChapterTitle()});
-                    } else if (!chaptersToBeUpdated.containsLink(chaptersInMemory.getChapterByTitle(row.getChapterTitle()).getLink())) {
+                    } //If the title is the same but the link is different
+                    else if (!chaptersToBeUpdated.containsLink(chaptersInMemory.getChapterByTitle(row.getChapterTitle()).getLink())) {
                         statement.addBatch("UPDATE novel_chapters SET link = '"
                                 + chaptersInMemory.getChapterByTitle(row.getChapterTitle()).getLink() + "'"
                                 + "WHERE link = '"
@@ -104,10 +123,9 @@ public class Database {
                         chaptersToBeUpdated.add(chaptersInMemory.getChapterByTitle(row.getChapterTitle()));
                         Scraper.LOGGER.log(Level.FINEST, "{0}: {1} The link was queued to be updated.", new Object[]{row.getNovelTitle(), row.getChapterTitle()});
                     }
-                } else if (
-                        chaptersInMemory.contains(Integer.valueOf(row.getChapterTitle().split("-")[0].trim())) 
-                        && 
-                        !chaptersToBeUpdated.containsLink(chaptersInMemory.getChapterByDigits(Integer.valueOf(row.getChapterTitle().split("-")[0].trim())).getLink())) {
+                } //If the digits are the same as the scraped chapter but the link is different
+                else if (chaptersInMemory.contains(Integer.valueOf(row.getChapterTitle().split("-")[0].trim()))
+                        && !chaptersToBeUpdated.containsLink(chaptersInMemory.getChapterByDigits(Integer.valueOf(row.getChapterTitle().split("-")[0].trim())).getLink())) {
                     Chapter chapter = chaptersInMemory.
                             getChapterByDigits(Integer.valueOf(row.getChapterTitle().split("-")[0].trim()));
                     statement.addBatch("UPDATE novel_chapters SET link = '"
@@ -148,13 +166,14 @@ public class Database {
                 results.close();
             }
 
-            List<String> dBNovels = new ArrayList<>();
+            List<String> dbNovelTitles = new ArrayList<>();
             ChapterContainer novelChapters = new ChapterContainer();
-            rows.forEach(row -> dBNovels.add(row.getNovelTitle().trim()));
+            rows.forEach(row -> dbNovelTitles.add(removeExtra(row.getNovelTitle())));
 
             for (Novel novel : novelsInMemory) {
                 novelChapters.clear();
-                if (!dBNovels.contains(novel.getTitle())) {
+                //If database doesn't contain a novel with this base title, insert it
+                if (!dbNovelTitles.contains(novel.getBaseTitle())) {
                     statement.addBatch("INSERT INTO"
                             + novelIdTable
                             + "VALUES ('"
@@ -164,7 +183,10 @@ public class Database {
                 }
 
                 for (var row : rows) {
-                    if (row.getNovelTitle().equals(novel.getTitle())) {
+                    if (removeExtra(row.getNovelTitle()).equals(novel.getBaseTitle())) {
+                        if (row.getNovelTitle().contains("Trafford")) {
+                            System.out.println("");
+                        }
                         novelChapters.add(new Chapter(row.getChapterTitle(), row.getChapterLink()));
                     }
                 }
